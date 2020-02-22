c4a58f2725b719267883a86807119f68c812e680
Abraham Arce <xe1gyq@gmail.com>
2020-02-22 13:31:11 -0600

    OpenVINO: Application Action Recognition
    Action Recognition application, OpenVINO based but kept as a
    separate application.
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- action-recognition/main.sh
+++ action-recognition/main.sh
@@ -23,15 +23,15 @@ cd $HOME
 source $OPENVINO_INSTALLATION/bin/setupvars.sh
 
 python3 main.py \
-        --encoder /home/user/Transportation/action_recognition/driver_monitoring/mobilenetv2/encoder/dldt/driver-action-recognition-adas-0002-encoder.xml \
-        --decoder /home/user/Transportation/action_recognition/driver_monitoring/mobilenetv2/decoder/dldt/driver-action-recognition-adas-0002-decoder.xml \
+        --encoder /home/user/intel/driver-action-recognition-adas-0002-encoder/FP32/driver-action-recognition-adas-0002-encoder.xml \
+        --decoder /home/user/intel/driver-action-recognition-adas-0002-decoder/FP32/driver-action-recognition-adas-0002-decoder.xml \
         --cpu_extension /opt/intel/openvino/inference_engine/lib/intel64/libcpu_extension_sse4.so \
-        --video ${VIDEO} \
+        --video ${INPUT} \
         --fps 30 \
         --labels /home/user/driver_actions.txt \
         --device ${DEVICE} \
-        --identification "${ID}" \
+        --identification ${ID} \
         --sink_fps 30 \
         --sink_fourcc MJPG \
-        --sink_ip ${SINK_IP} \
-        --sink_port ${SINK_PORT}
+        --sink_ip ${SINK_HOST_IP} \
+        --sink_port ${SINK_HOST_PORT}
