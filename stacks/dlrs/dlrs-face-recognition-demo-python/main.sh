0bb74eea1157786f15caff30e11da57b675ee7c4
Abraham Arce <xe1gyq@gmail.com>
2020-03-07 08:55:48 -0600

    Stacks: DLRS Interactive Face Recognition Demo
    This example [0] demonstrates an approach to create interactive
    applications for video processing. It shows the basic architecture
    for building model pipelines supporting model placement on different
    devices and simultaneous parallel or sequential execution using
    OpenVINO library in Python.
    
    [0] https://github.com/opencv/open_model_zoo/tree/master/demos/python_demos/face_recognition_demo
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ stacks/dlrs/dlrs-face-recognition-demo-python/main.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+export MQTT_SERVER=172.17.0.1:1883
+export MQTT_CLIENT_ID=dlrs-face-recognition-demo-python
+
+export MODEL_FACE_DETECTION=/Retail/object_detection/face/sqnet1.0modif-ssd/0004/dldt/
+export MODEL_FACIAL_LANDMARKS_REGRESSION=/Retail/object_attributes/landmarks_regression/0009/dldt/
+export MODEL_FACE_REIDENTIFICATION=/Retail/object_reidentification/face/mobilenet_based/dldt/
+
+export FACES=/workspace/media/faces/
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+source /.bashrc 
+
+case $TARGET in
+
+     CPU)
+         TARGET='-d=CPU'
+         FP='FP32'
+	 EXTENSION='-l /usr/local/lib/inference-engine/libcpu_extension.so'
+         ;;
+
+esac
+
+cd /workspace/open_model_zoo/demos/python_demos/face_recognition_demo/
+
+python3 ./face_recognition_demo.py \
+  -m_fd $MODEL_FACE_DETECTION/face-detection-retail-0004.xml \
+  -m_lm $MODEL_FACIAL_LANDMARKS_REGRESSION/landmarks-regression-retail-0009.xml \
+  -m_reid $MODEL_FACE_REIDENTIFICATION/face-reidentification-retail-0095.xml \
+  $EXTENSION \
+  -fg $FACES \
+  -i $INPUT
