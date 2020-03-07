c486fcb39bcf78639366e8faa3170c6b06861e3e
Abraham Arce <xe1gyq@gmail.com>
2020-03-07 08:17:45 -0600

    Stacks: DLRS Docker Compose + Makefile
    Based in our architecture proposal, we need a Docker Compose file
    and a Makefile.
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- stacks/dlrs/dlrs-restricted-zone-notifier-python/main.sh
+++ stacks/dlrs/dlrs-restricted-zone-notifier-python/main.sh
@@ -4,7 +4,10 @@
 # Variables
 # =============================================================================
 
-OPENVINO_INSTALLATION=/opt/intel/openvino/
+export MQTT_SERVER=172.17.0.1:1883
+export MQTT_CLIENT_ID=restricted-zone-notifier-python
+
+export MODEL=/Retail/object_detection/pedestrian/rmnet_ssd/0013/dldt
 
 # =============================================================================
 # Functions
@@ -18,16 +21,12 @@ OPENVINO_INSTALLATION=/opt/intel/openvino/
 
 source /.bashrc 
 
-export MQTT_SERVER=172.17.0.1:1883
-export MQTT_CLIENT_ID=restricted-zone-notifier-python
-export MODEL=/Retail/object_detection/pedestrian/rmnet_ssd/0013/dldt
-
 case $TARGET in
 
      CPU)
          TARGET='-d=CPU'
          FP='FP32'
-	 EXTENSION=/usr/local/lib/inference-engine/libcpu_extension.so
+	 EXTENSION='-l /usr/local/lib/inference-engine/libcpu_extension.so'
          ;;
 
 esac
@@ -36,9 +35,7 @@ if [[ $INPUT ]]; then
    sed -i "6s|.*|\"video\":\"${INPUT}\"|" /workspace/restricted-zone-notifier-python/resources/config.json
 fi
 
-cat /workspace/restricted-zone-notifier-python/resources/config.json
-
 cd /workspace/restricted-zone-notifier-python/application/
 python3 restricted_zone_notifier.py \
   -m $MODEL/person-detection-retail-0013.xml \
-  -l $EXTENSION
+  $EXTENSION
