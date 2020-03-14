c9e857f9fcae4a1c404eb96e26f84f250dba9301
Abraham Arce <xe1gyq@gmail.com>
2020-03-14 16:00:58 -0600

    OpenVINO: Interactive Face Recognition Demo
    This example [0] demonstrates an approach to create interactive applications for video processing.
    It shows the basic architecture for building model pipelines supporting model placement on
    different devices and simultaneous parallel or sequential execution using OpenVINO library in
    Python.
    
    [0] https://docs.openvinotoolkit.org/latest/_demos_python_demos_face_recognition_demo_README.html
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ openvino/interactive-face-recognition-demo-python/main.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+OPENVINO_INSTALLATION=/opt/intel/openvino/
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+cd $HOME
+
+source $OPENVINO_INSTALLATION/bin/setupvars.sh
+
+export MQTT_SERVER=172.17.0.1:1883
+export MQTT_CLIENT_ID=restricted-zone-notifier-cpp
+
+case $TARGET in
+
+     CPU)
+         TARGET='-d_fd CPU -d_lm CPU -d_reid CPU'
+         FP='FP32'
+         ;;
+
+     GPU)
+         TARGET='-d_fd GPU -d_lm GPU -d_reid GPU'
+         FP='FP16'
+         ;;
+
+     MYRIAD)
+         TARGET='-d_fd MYRIAD -d_lm MYRIAD -d_reid MYRIAD'
+         FP='FP16'
+         ;;
+esac
+
+cd /opt/intel/openvino/deployment_tools/open_model_zoo/demos/python_demos/face_recognition_demo
+python3 ./face_recognition_demo.py \
+  -m_fd /home/user/intel/face-detection-retail-0004/${FP}/face-detection-retail-0004.xml \
+  -m_lm /home/user/intel/landmarks-regression-retail-0009/${FP}/landmarks-regression-retail-0009.xml \
+  -m_reid /home/user/intel/face-reidentification-retail-0095/${FP}/face-reidentification-retail-0095.xml \
+  -fg /tmp/demo/ \
+  $TARGET \
+  -i $INPUT
