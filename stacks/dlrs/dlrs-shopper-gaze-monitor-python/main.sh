4814cd045d1d679ef303256776ba771020b025e2
Abraham Arce <xe1gyq@gmail.com>
2020-06-18 22:37:30 -0500

    Stacks: DLRS Shopper Gaze Monitor +
    Use a retail shelf-mounted camera system to count passersby that
    look toward the display compared to the number of people that
    pass without looking. It is intended to provide real-world marketing
    statistics for in-store shelf-space advertising [0].
    
    + some other required changes.
    
    [0] https://software.intel.com/content/www/us/en/develop/topics/iot/reference-implementations/shopper-gaze-monitor.html
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- stacks/dlrs/dlrs-shopper-gaze-monitor-python/main.sh
+++ stacks/dlrs/dlrs-shopper-gaze-monitor-python/main.sh
@@ -7,7 +7,8 @@
 export MQTT_HOST=172.17.0.1
 export MQTT_CLIENT_ID=restricted-zone-notifier-python
 
-export MODEL=/Retail/object_detection/pedestrian/rmnet_ssd/0013/dldt
+export MODEL_M=/Transportation/object_detection/face/pruned_mobilenet_reduced_ssd_shared_weights/dldt/
+export MODEL_PM=/Transportation/object_attributes/headpose/vanilla_cnn/dldt/
 
 # =============================================================================
 # Functions
@@ -30,7 +31,7 @@ case $TARGET in
 esac
 
 if [[ $INPUT ]]; then
-   sed -i "6s|.*|\"video\":\"${INPUT}\"|" /workspace/restricted-zone-notifier-python/resources/config.json
+   sed -i "6s|.*|\"video\":\"${INPUT}\"|" /workspace/shopper-gaze-monitor-python/resources/config.json
 fi
 
 echo "Host         : " $HOSTNAME
@@ -38,6 +39,7 @@ echo "Input        : " $INPUT
 echo "Target       : " $TARGET
 echo "Statsd Server: " $STATSD_SERVER
 
-cd /workspace/restricted-zone-notifier-python/application/
-python3 restricted_zone_notifier.py \
-  -m $MODEL/person-detection-retail-0013.xml \
+cd /workspace/shopper-gaze-monitor-python/application/
+python3 shopper_gaze_monitor.py \
+  -m $MODEL_M/face-detection-adas-0001.xml \
+  -p $MODEL_PM/head-pose-estimation-adas-0001.xml
