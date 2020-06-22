61b6e495b1896223a77ac562efe4e8e023957cf9
Abraham Arce <xe1gyq@gmail.com>
2020-06-22 10:10:47 -0500

    OpenVINO: Restricted ... Argument Model
    Let's specify the model as argument in order to test the different
    offerings.
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- openvino/restricted-zone-notifier-python/main.sh
+++ openvino/restricted-zone-notifier-python/main.sh
@@ -44,15 +44,36 @@ if [[ $INPUT ]]; then
    sed -i "6s|.*|\"video\":\"${INPUT}\"|" /home/user/restricted-zone-notifier-python/resources/config.json
 fi
 
+case $MODEL in
+
+      person-detection-retail-0013)
+          MODEL_ROOT_PATH=/opt/intel/openvino/deployment_tools/open_model_zoo/tools/downloader/intel
+          MODEL_PATH=$MODEL_ROOT_PATH/$MODEL/${FP}/$MODEL.xml
+          ;;
+
+      pedestrian-detection-adas-0002)
+          MODEL_ROOT_PATH=/home/user/intel
+          MODEL_PATH=$MODEL_ROOT_PATH/$MODEL/${FP}/$MODEL.xml
+          ;;
+
+      pedestrian-detection-adas-binary-0001)
+          MODEL_ROOT_PATH=/home/user/intel
+          MODEL_PATH=$MODEL_ROOT_PATH/$MODEL/FP32-INT1/$MODEL.xml
+          ;;
+
+esac
+
 echo "Host         : " $HOSTNAME
 echo "Input        : " $INPUT
 echo "Target       : " $TARGET
+echo "Model        : " $MODEL
+echo "Model Path   : " $MODEL_PATH
 echo "Threshold    : " $THRESHOLD
 echo "MQTT Server  : " $MQTT_SERVER
 echo "Statsd Server: " $STATSD_SERVER
 
 cd /home/user/restricted-zone-notifier-python/application/
 python3 restricted_zone_notifier.py \
-  -m=/opt/intel/openvino/deployment_tools/open_model_zoo/tools/downloader/intel/person-detection-retail-0013/${FP}/person-detection-retail-0013.xml \
+  -m=$MODEL_PATH \
   $TARGET \
   -th $THRESHOLD
