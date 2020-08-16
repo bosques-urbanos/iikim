62deb2da2e7c7426c10e4f70c9a95e77e2fd8c0a
Abraham Arce <xe1gyq@gmail.com>
2020-08-16 14:45:32 -0500

    OpenVINO: User Identification
    We are generating media with root permissions, this is causing an extra
    step (chwon'ing the files) for later phases.
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- openvino/interactive-face-recognition-demo-python/main.sh
+++ openvino/interactive-face-recognition-demo-python/main.sh
@@ -52,6 +52,6 @@ python3 ./face_recognition_demo.py \
   -m_fd /home/user/intel/face-detection-retail-0004/${FP}/face-detection-retail-0004.xml \
   -m_lm /home/user/intel/landmarks-regression-retail-0009/${FP}/landmarks-regression-retail-0009.xml \
   -m_reid /home/user/intel/face-reidentification-retail-0095/${FP}/face-reidentification-retail-0095.xml \
-  -fg /tmp/demo/ \
+  -fg /tmp/people/ --run_detector --allow_grow \
   $TARGET \
   -i $INPUT
